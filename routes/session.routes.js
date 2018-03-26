0811a80870b1ff074e6c453e1faf908dee16d8f8
Pablo Laso <plasocortabitarte@gmail.com>
2018-03-26 18:14:32 +0200

    feat: users and session routes-controllers

--- routes/session.routes.js
+++ routes/session.routes.js
@@ -3,4 +3,7 @@ const router = express.Router();
 const secureMiddleware = require('../middleware/secure.middleware');
 const sessionController = require('../controllers/session.controller');
 
+router.post('/', sessionController.create);
+router.delete('/', sessionController.destroy);
+
 module.exports = router;
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -2,4 +2,6 @@ const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/users.controller');
 
+router.post('/', usersController.create);
+
 module.exports = router;
