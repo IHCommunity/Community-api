ba908b43e213ac262b9cc404003698f6e603dded
Pablo Laso <plasocortabitarte@gmail.com>
2018-03-26 17:47:34 +0200

    proyect scaffolding and configs, fixes #1, #2, #3, #4, #5

---
+++ models/api-error.model.js
@@ -0,0 +1,10 @@
+class ApiError {
+
+  constructor(message, status = 400, errors = {}) {
+      this.message = message;
+      this.status = status;
+      this.errors = errors;
+  }
+}
+
+module.exports = ApiError;
