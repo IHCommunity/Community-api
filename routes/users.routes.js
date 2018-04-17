17d6aa0dd6a922fbe96b4fe6f07aa0b28d0a971e
Pablo Laso <plasocortabitarte@gmail.com>
2018-04-17 11:51:28 +0200

    feat: users crud

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -6,6 +6,8 @@ router.post('/', usersController.create);
 router.get('/', usersController.list);
 router.post('/check', usersController.check);
 router.get('/:id', usersController.get);
+router.put('/:id', usersController.edit);
 router.put('/:id/latch', usersController.pairLatch);
+router.get('/:id/latch', usersController.unpairLatch);
 
 module.exports = router;
