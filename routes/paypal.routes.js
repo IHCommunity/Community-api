4366e7bda0d7c6619a1ca6d6a13743d0fcbe131e
Marco <marco238cuba@gmail.com>
2018-04-17 21:42:32 +0200

    feat: dinamic paypal`s data

--- routes/paypal.routes.js
+++ routes/paypal.routes.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const paypalController = require('../controllers/paypal.controller');
 
-router.post('/', paypalController.createPayment);
-router.get('/succes', paypalController.makePayment);
+router.post('/:id', paypalController.createPayment);
+router.get('/success/:amount', paypalController.makePayment);
 router.get('/cancel', paypalController.cancelPayment);
 
 module.exports = router;
