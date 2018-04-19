f68b0a36941f334b95067623f4be8b92cfc827cd
Pablo Laso <plasocortabitarte@gmail.com>
2018-04-19 22:39:33 +0200

    fix: user routes

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -4,9 +4,9 @@ const usersController = require('../controllers/users.controller');
 const upload = require('../configs/multer.config');
 const secureMiddleware = require('../middleware/secure.middleware');
 
-router.post('/', secureMiddleware.isAuthenticated, usersController.create);
+router.post('/', usersController.create);
 router.get('/', secureMiddleware.isAuthenticated, usersController.list);
-router.post('/check', secureMiddleware.isAuthenticated, usersController.check);
+router.post('/check', usersController.check);
 router.get('/:id', secureMiddleware.isAuthenticated, usersController.get);
 router.put('/:id', secureMiddleware.isAuthenticated, usersController.edit);
 router.post('/:id', secureMiddleware.isAuthenticated, upload.single('avatar'), usersController.edit);
